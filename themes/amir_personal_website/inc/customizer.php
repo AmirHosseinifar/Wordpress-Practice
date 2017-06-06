140f4efcc4fa1220ed43ad5088ef349f0c996293
AmirHosseinifar <hosseinifar.amir@gmail.com>
2017-06-06 14:52:28 -0700

    Wordpress Practice

---
+++ themes/amir_personal_website/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * AmirPersonal Theme Customizer
+ *
+ * @package AmirPersonal
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function amirpersonal_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'amirpersonal_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function amirpersonal_customize_preview_js() {
+	wp_enqueue_script( 'amirpersonal_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'amirpersonal_customize_preview_js' );
