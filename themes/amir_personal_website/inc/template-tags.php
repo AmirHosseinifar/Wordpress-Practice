bf3e03e9c4f08c82cde534b5b6ce8cbf415efab3
AmirHosseinifar <hosseinifar.amir@gmail.com>
2017-06-11 22:52:26 -0700

    relocated category

--- themes/amir_personal_website/inc/template-tags.php
+++ themes/amir_personal_website/inc/template-tags.php
@@ -44,13 +44,8 @@ if ( ! function_exists( 'amirpersonal_entry_footer' ) ) :
  * Prints HTML with meta information for the categories, tags and comments.
  */
 function amirpersonal_entry_footer() {
-	// Hide category and tag text for pages.
+	// Hide tag text for pages.
 	if ( 'post' === get_post_type() ) {
-		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( esc_html__( ', ', 'amirpersonal' ) );
-		if ( $categories_list && amirpersonal_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'amirpersonal' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-		}
 
 		/* translators: used between list items, there is a space after the comma */
 		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'amirpersonal' ) );
@@ -78,6 +73,17 @@ function amirpersonal_entry_footer() {
 }
 endif;
 
+/** 
+* Display category list
+*/
+
+function amirpersonal_the_category_list() {
+		/* translators: used between list items, there is a space after the comma */
+		$categories_list = get_the_category_list( esc_html__( ', ', 'amirpersonal' ) );
+		if ( $categories_list && amirpersonal_categorized_blog() ) {
+			printf( '<span class="cat-links">' . esc_html__( '%1$s', 'amirpersonal' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+		}
+}
 /**
  * Returns true if a blog has more than 1 category.
  *
