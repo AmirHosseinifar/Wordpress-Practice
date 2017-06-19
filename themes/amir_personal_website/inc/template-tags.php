99aab1272ed7f24b3769b9272efacd99935890fb
AmirHosseinifar <hosseinifar.amir@gmail.com>
2017-06-19 13:10:54 -0700

    upadeted metadata with responsive design

--- themes/amir_personal_website/inc/template-tags.php
+++ themes/amir_personal_website/inc/template-tags.php
@@ -30,7 +30,7 @@ function amirpersonal_posted_on() {
 	);
 
 	$byline = sprintf(
-		esc_html_x( 'Wriiten by %s', 'post author', 'amirpersonal' ),
+		esc_html_x( 'Written by %s', 'post author', 'amirpersonal' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
