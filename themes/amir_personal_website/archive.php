d00284778915ce7bc5695c634caa397b296f66d5
AmirHosseinifar <hosseinifar.amir@gmail.com>
2017-06-22 17:08:53 -0700

    Customized search result and 404 error page

--- themes/amir_personal_website/archive.php
+++ themes/amir_personal_website/archive.php
@@ -8,6 +8,7 @@
  */
 
 get_header(); ?>
+
 <?php
 if ( have_posts() ) : ?>
 
@@ -18,14 +19,18 @@ if ( have_posts() ) : ?>
 				?>
 	</header><!-- .page-header -->
 
-<?php endif; ?>
+<?php 
+else :
+
+	get_template_part(
+		'template-parts/content', 'none' );
+		return;
+	
+	endif; ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-		<?php
-		if ( have_posts() ) : ?>
-
+			
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
@@ -45,12 +50,7 @@ if ( have_posts() ) : ?>
 				'before_page_number' => '<span class="screen-reader-text">' . __('page ', 'humescores' ) . '</span>',
 			));
 
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
+		?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
--- themes/amir_personal_website/search.php
+++ themes/amir_personal_website/search.php
@@ -9,15 +9,21 @@
 
 get_header(); ?>
 
+<?php
+if ( have_posts() ) : ?>
+	<header class="page-header">
+		<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'amirpersonal' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+	</header><!-- .page-header -->
+
+<?php 
+else :
+	get_template_part( 'template-parts/content', 'none' );
+	return;
+
+endif; ?>
+
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-		<?php
-		if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'amirpersonal' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
 
 			<?php
 			/* Start the Loop */
@@ -28,17 +34,17 @@ get_header(); ?>
 				 * If you want to overload this in a child theme then include a file
 				 * called content-search.php and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', 'search' );
+				get_template_part( 'template-parts/content' );
 
 			endwhile;
 
-			the_posts_navigation();
+			the_posts_pagination( array(
+				'prev_text' =>__( 'Newer', 'amirpersonal'),
+				'next_text' =>__( 'Older', 'amirpersonal'),
+				'before_page_number' => '<span class="screen-reader-text">' . __('page ', 'humescores' ) . '</span>',
+			));
 
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
+		?>
 
 		</main><!-- #main -->
 	</section><!-- #primary -->
